9adf134a6894238727c5d7001ef57e432bb757b0
Abel Atkinson <Jetforce2@gmail.com>
2020-10-21 07:13:51 -0500

    Added a database member to the user class

--- PersonalPasswordManager/User.h
+++ PersonalPasswordManager/User.h
@@ -1,13 +1,11 @@
 #ifndef USER_H
 #define USER_H
 
+#define MAXNAMESZ	64
+#define MAXPWSZ 512
 
-
-#define MAXNAMESZ	32
-#define MAXPWSZ	512
-
-#include "Database.h"
 #include "Hash.h"
+#include "Database.h"
 
 class User
 {
@@ -18,33 +16,12 @@ public:
 	void	setLastName(const char* last);
 	void	setUsername(const char* user);
 	void	setPassword(const char* pass);
-	
-private:
-	char	firstName[MAXNAMESZ + 1];
-	char	lastName[MAXNAMESZ + 1];
-	Hash*	usernameHash;
-	Hash*	passwordHash;
-	
-};
 
-#if 0
-class User
-{
-public:
-	User();
-	~User();
-	void	setFirstName(const char* first);
-	void	setLastName(const char* last);
-	void	setUsername(const char* user);
-	void	setPassword(const char* pass);
-	void	initiateDatabase();
 private:
 	char			firstName[MAXNAMESZ + 1];
 	char			lastName[MAXNAMESZ + 1];
-	char			username[MAXNAMESZ + 1];
-	char			password[MAXPWSZ + 1];
+	Hash*			usernameHash;
+	Hash*			passwordHash;
 	Database*	db;
 };
-#endif
-
 #endif // USER_H
