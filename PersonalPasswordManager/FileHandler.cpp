bfbdb00a98a2bb73a2d2728b453edfa7bc54f932
Abel Atkinson <Jetforce2@gmail.com>
2020-10-23 04:11:06 -0500

    Incomplete definitions of the class prototypes

--- PersonalPasswordManager/FileHandler.cpp
+++ PersonalPasswordManager/FileHandler.cpp
@@ -1,16 +1,97 @@
+
+
+
 #include "FileHandler.h"
+#include <Windows.h>
+#include <io.h>
+#include <fcntl.h>
+
+#if defined(UNIX) || defined(__unix__) || defined(__linux__) || defined(LINUX)
+#include <unistd.h>
+
+#endif
 
 
-FileHandler::FileHandler()
+#if defined(WIN32) || defined(_WIN32)
+#define O_CREAT	_O_CREAT
+#define O_RDWR	_O_RDWR
+
+#endif
+
+
+/*
+ * Date: 10/23/20
+ * NOTE:
+ * In the process of making this windows application compatible with linux
+ * in the close future I will probably make dumb/odd decisions with what
+ * is implemented and what isn't, up until I design something to be more 
+ * aligned with a decently designed windows/linux application.
+ * For now anything that I attempt to create a linux solution for will
+ * be simply because I remembered I can do this or that...sorry
+ */
+FileHandler::FileHandler(int type)
 {
-	fh = NULL;
+	fh			= NULL;
+	fd			= 0;
+	fileType	= type;
+	memset(fileName, 0, sizeof(fileName));
 	memset(fileHandlerPath, 0, sizeof(fileHandlerPath));
 }
+
+
 
 FileHandler::~FileHandler()
 {
-	if (fh)
-		fclose(fh);
+	if (fd)
+		BinClose();
 	memset(fileHandlerPath, 0, sizeof(fileHandlerPath));
 }
 
+
+void FileHandler::BinOpen(const char* pszPath)
+{
+	if (fileType == TYPE_BIN)
+	{
+		fd = open(pszPath, O_CREAT)
+	}
+}
+
+void FileHandler::BinClose()
+{
+	if (fileType == TYPE_BIN && fd > 0)
+		close(fd);
+}
+
+
+void FileHandler::BinRead()
+{
+	if (fileType == TYPE_BIN)
+	{
+
+	}
+}
+
+void FileHandler::BinAppendWrite()
+{
+	if (fileType == TYPE_BIN)
+	{
+
+	}
+}
+
+
+void FileHandler::TextRead()
+{
+	if (fileType == TYPE_TEXT)
+	{
+
+	}
+}
+
+void FileHandler::TextWrite()
+{
+	if (fileType == TYPE_TEXT)
+	{
+
+	}
+}
