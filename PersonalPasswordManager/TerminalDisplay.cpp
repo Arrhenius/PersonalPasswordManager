f6ab4b1caed93453b79cee1b9eb1c92bdf867a2d
Abel Atkinson <Jetforce2@gmail.com>
2020-10-23 04:13:28 -0500

    Slowly building the menu

--- PersonalPasswordManager/TerminalDisplay.cpp
+++ PersonalPasswordManager/TerminalDisplay.cpp
@@ -50,7 +50,34 @@ void Display::LoginMenu()
 	using std::cout;
 	using std::endl;
 
+	
+	char username[MAXNAMESZ + 1];
+	char password[MAXPWSZ + 1];
+
+	// Will clear these variables twice in the function.
+	// One before to zero out the memory and the second
+	// to make sure entered information is destroyed once 
+	// used
+	memset(username, 0, sizeof(username));
+	memset(password, 0, sizeof(password));
+
 	system("cls");
+
+	
+	printf("Username: ");
+	fgets(username, MAXNAMESZ, stdin);
+	printf("Password: ");
+	fgets(password, MAXPWSZ, stdin);
+
+
+	SetUserHash(username);
+	SetKeyHash(password);
+	memset(username, 0, sizeof(username));
+	memset(password, 0, sizeof(password));
+	if (userLoginName && userLoginKey)
+	{
+		CheckSave();
+	}
 	
 
 }
